66053a04779ee9f77a8dd3877bdadfdcf17c851e
helitonns <helitonns@gmail.com>
2025-04-29 20:06:48 -0400

    Utilizado o immer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useReducer, useState } from "react";
-import { ActionTypes, Cycle, cycleReducer } from "../reducers/cycles";
+import { Cycle, cycleReducer } from "../reducers/cycles/reducer";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/action";
 
 interface CreateCycleData {
   task: string;
@@ -42,12 +43,7 @@ export function CyclesContextProvider({ children } : CyclesContextProviderProps)
   }
 
   function markCurrentCycleAsFinished() {
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(markCurrentCycleAsFinishedAction());
   }
   
   function createNewCycle(data: CreateCycleData) {
@@ -58,23 +54,13 @@ export function CyclesContextProvider({ children } : CyclesContextProviderProps)
       startDate: new Date(),
     };
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      },
-    });
+    dispatch(addNewCycleAction(newCycle)); 
 
     setAmountSecondsPassed(0); 
   }
 
   function interruptCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      },
-    });
+    dispatch(interruptCurrentCycleAction()); 
   }
 
   return (
